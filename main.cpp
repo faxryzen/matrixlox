3c4ccb8671ff2847d17d0c0bc72fee9076be24f5
faxryzen <cheburek2006@gmail.com>
2025-11-01 15:03:49 +0300

    Created construct, input and output

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int ** create(size_t rows, size_t cols)
   size_t created = 0;
   try 
   {
-    for (; created < rows; ++created) //(size_t i = 0; i < rows; ++i)
+    for (; created < rows; ++created)
     {
       mtx[created] = new int[cols];
     }
@@ -28,19 +28,69 @@ int ** create(size_t rows, size_t cols)
   return mtx;
 }
 
+void construct(int ** mtx, int init, size_t rows, size_t cols)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < cols; ++j)
+    {
+      mtx[i][j] = init;
+    }
+  }
+}
+
+void input(int ** mtx, size_t rows, size_t cols)
+{
+  for (size_t i = 0; i < rows; i++)
+  {
+    for (size_t j = 0; j < cols; j++)
+    {
+      std::cin >> mtx[i][j];
+    }
+  }
+}
+void output(int ** mtx, size_t rows, size_t cols)
+{
+  for (size_t i = 0; i < rows; ++i)
+  {
+    std::cout << mtx[i][0];
+    for (size_t j = 1; j < cols; ++j)
+    {
+      std::cout << ' ' << mtx[i][j];
+    }
+    std::cout << '\n';
+  }
+}
+
+
 int main()
 {
-  int ** matrix = nullptr;
-  try
+  size_t r = 0, c = 0;
+  std::cin >> r >> c;
+  if (!std::cin)
   {
-    matrix = create(5,5);
-  }
-  catch (const std::bad_alloc & e)
-  {
-    std::cerr << e.what() << '\n';
+    std::cerr << "ERROR: input failed!\n";
 
     return 1;
   }
-  std::cout << matrix[0] << '\n'; //UB??
-  destroy(matrix, 5);
+
+  int ** matrix = create(r, c); //можно обернуть в try/catch
+
+  std::cout << "Matrix created!\n";
+
+  construct(matrix, 0, r, c); //инициализация
+
+  std::cout << "Enter matrix elements:\n";
+
+  input(matrix, r, c);
+  if (!std::cin)
+  {
+    destroy(matrix, r);
+    std::cerr << "ERROR: input failed!\n";
+    return 1;
+  }
+  std::cout << "Output:\n";
+  output(matrix, r, c);
+
+  destroy(matrix, r);
 }
